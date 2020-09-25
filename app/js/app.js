efdcc6c5f3f10a115deb7e1ff5af9b10f4073312
Ivan Gorbunov <i.gorbunov@corp.101xp.com>
2020-09-25 14:52:31 +0300

    add lasyload

--- app/js/app.js
+++ app/js/app.js
@@ -608,12 +608,12 @@ const videoPause = (videoClass) => {
 };
 
 const initProductSlider = () => {
-  $('.product-gallery-slider').on('init', () => {
-    videoPlay('.slider-video');
-  });
-  $('.product-gallery-slider').on('reInit', function (slick) {
-    videoPlay('.slider-video');
-  });
+  // $('.product-gallery-slider').on('init', () => {
+  //   videoPlay('.slider-video');
+  // });
+  // $('.product-gallery-slider').on('reInit', function (slick) {
+  //   videoPlay('.slider-video');
+  // });
 
   $('.product-gallery-slider').slick({
     slidesToShow: 1,
